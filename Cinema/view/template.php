405402634a47c6ae8a51666285c0f39cdd8b789e
EnesCIL <enescil2003@hotmail.com>
2025-12-17 16:41:48 +0100

    acceptation changement

--- Cinema/view/template.php
+++ Cinema/view/template.php
@@ -3,17 +3,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-=======
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
->>>>>>> febbed456577a74a5c5a2f58d276711c6db4a411
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title><?= $titre ?></title>
 </head>
 <body>
-<<<<<<< HEAD
     <nav class="uk-navbar-container" uk-navbar uk-striped></nav>
     <!-- Le titre secondaire et le contenu principal (dans listFilms) -->
     <div id="wrapper" class="uk-container-expand">
@@ -21,13 +16,6 @@
             <div id ="contenu">
                 <h1 class="uk-heading-divider">PDO Cinema</h1>
                 <h2 class="uk-heading-bullet"><?= $titre_secondaire?></h2>
-=======
-    <div id="wrapper id="wrapper" class="uk-container uk-container-expand">
-        <main>
-            <div id="contenu">
-                <h1 class="uk-heading-divider">PDO Cinema</h1>
-                <h2 class="uk-heading-bullet"><?= $titre_secondaire ?></h2>
->>>>>>> febbed456577a74a5c5a2f58d276711c6db4a411
                 <?= $contenu ?>
             </div>
         </main>
